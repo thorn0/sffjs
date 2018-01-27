138ea43e6bdc6462bdb75f58af606fec94b40165
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2018-01-27 02:30:46 +0200

    v1.13.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,24 +35,6 @@ module.exports = function(grunt) {
                     }
                 }
             },
-            tests: {
-                files: [{
-                    src: 'src/tests.html',
-                    dest: 'build/test/tests.html',
-                }, {
-                    src: 'src/stringformat.tests.js',
-                    dest: 'build/test/tests.js'
-                }],
-                options: {
-                    process: function(content) {
-                        return content
-                            .replace('stringformat.js', 'sffjs.js')
-                            .replace('stringformat.tests.js', 'tests.js')
-                            .replace(/src="(?!tests\.js)/g, 'src="../')
-                            .replace(/\r/g, '');
-                    }
-                }
-            },
             extras: {
                 files: [{
                     src: ['bower.json', 'package.json', 'readme.md', 'LICENSE', 'changelog.txt'],
